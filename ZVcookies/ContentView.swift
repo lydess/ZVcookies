119e6f0487f58ab4375dc0f869eb72e10729bfbb
william <lightbekon@gmail.com>
2022-01-30 12:24:12 +1100

    Initial Commit

---
+++ ZVcookies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZVcookies
+//
+//  Created by william Vise on 30/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZVcookies/ZVcookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZVcookiesApp.swift
+//  ZVcookies
+//
+//  Created by william Vise on 30/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZVcookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
